c5c4502f03ac146ccca3b55d86a93a4493197049
littledu <littledu@tencent.com>
2016-07-02 14:56:04 +0800

    1.3.3

--- build/upload.js
+++ build/upload.js
@@ -8,26 +8,24 @@ const http = require('http');
 const fs = require('fs');
 const del = require('del');
 const gulp = require('gulp');
-const zip = require('gulp-zip');
+const rename = require('gulp-rename');
 const config = require('rc')('qiniu');
 
 let weflowPath = path.join(__dirname, '../');
-let srcAll = path.join(weflowPath, 'dist', '**/*');
+let distZip = path.join(weflowPath, 'dist.zip');
 let pkg = require(path.join(weflowPath, 'package.json'));
 let distName = `WeFlow-${pkg.version}-${process.platform}-${process.arch}.zip`;
 
-distName = 'dist.zip';
-
 async.series([
-    // function (next) {
-    //     gulp.src(srcAll, {dot: true})
-    //         .pipe(zip(distName))
-    //         .pipe(gulp.dest(weflowPath))
-    //         .on('end', function () {
-    //             console.log('zip success.');
-    //             next();
-    //         });
-    // },
+    function (next) {
+        gulp.src(distZip)
+            .pipe(rename(distName))
+            .pipe(gulp.dest(weflowPath))
+            .on('end', function () {
+                console.log('rename success.');
+                next();
+            });
+    },
     function (next) {
         //准备上传
         qiniu.conf.ACCESS_KEY = config['ACCESS_KEY'];
